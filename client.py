1a2969b60c09097219009a80578e7aae5d492dc9
kwasiedwards <kwasiedwards@gmail.com>
2025-10-22 08:31:43 -0400

    updated client

--- client.py
+++ client.py
@@ -1,24 +1,29 @@
 from socket import socket, AF_INET,SOCK_STREAM
+import threading
+
 
 serverName = "localhost"
 serverPort=12009
 
 clientSocket = socket(AF_INET, SOCK_STREAM)
-
 clientSocket.connect((serverName,serverPort))
 
+def handle_server_messages(socket):
+    while True:
+        svrResp = clientSocket.recv(1024)
+        svrRespStr = svrResp.decode()
+        print ("Received from Server: ", svrRespStr)
+
+
+
+thread_handler = threading.Thread(target=handle_server_messages, args=(clientSocket,))
+thread_handler.start() 
+ 
 while 1:
-    numStr = input(f"(Input a number: ")
-
-    clientSocket.send(bytes(numStr, "utf-8"))
-
-    print ("Sent to Server: ", numStr)
-
-    svrResp = clientSocket.recv(1024)
-    svrRespStr = svrResp.decode()
-    print ("Received from Server: ", svrResp)
-
-    if svrRespStr == "You're correct":
+    msg = input(f"(Input (q to quit): ")
+    if msg == "q":
         break
+
+    clientSocket.send(bytes(msg, "utf-8"))
 
 clientSocket.close()
